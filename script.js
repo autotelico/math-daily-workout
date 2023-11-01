9991b053d22f2c9f6b3fb3b94f1b14e02f84ece4
Henrique Heron <henriqueheron1@gmail.com>
2023-10-31 23:44:25 -0300

    feat: add start game function

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const startButton = document.querySelector("#start-button")
+    const foretext = document.querySelector("#foretext");
+    const startButton = document.querySelector("#start-button");
+    const textResponse = document.querySelector("#text-response");
+    const userAnswerLabel = document.querySelector("label");
+    
+    function startGame() {
+        foretext.textContent = "";
+        startButton.style.display = "none";
+        userAnswerLabel.style.display = "none";
+        textResponse.textContent = "Memorize the result for the next operation...";
+    }
 
-
-
-
-    button.addEventListener('click', () => {
-        
+    startButton.addEventListener('click', () => {
+        startGame();
     })
 })
